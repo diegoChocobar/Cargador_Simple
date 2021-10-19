3d061aa979b4a00789cb8e0f5ad4305990d42d77
diegoChocobar <registros.d@gmail.com>
2021-10-19 19:13:04 -0300

    parpadeo led

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,8 @@ void loop() {
     digitalWrite(output_rele, HIGH);  
     digitalWrite(output_led, HIGH); 
     delay(1000);
+    digitalWrite(output_rele, LOW);  
+    digitalWrite(output_led, LOW); 
 
   
   }//end while(1)
