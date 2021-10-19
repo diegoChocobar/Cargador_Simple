6548b8457a1a35ff93aa48aa615b493aa6b74932
diegoChocobar <registros.d@gmail.com>
2021-10-19 19:09:18 -0300

    incio de proyecto

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+
+#include <Variables.h>
+#include <setup.h>
+
+
+
+void loop() {
+
+  while(1){
+
+    digitalWrite(output_rele, HIGH);  
+    digitalWrite(output_led, HIGH); 
+    delay(1000);
+
+  
+  }//end while(1)
+
+
+
+}
+
